636ecf9f74c95455b5fba697b2894ff050f5d80e
dengying07 <dengying0711@gmail.com>
2013-11-16 01:20:29 +0000

    lala

--- hogwarts.pl
+++ hogwarts.pl
@@ -1,6 +1,5 @@
-
-% the students in Hogwarts 
-% student(SID,SN,House)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% the students in Hogwarts %%%%%%%% student(SID,SN,House)
 student(hp, 'Harry James Potter', gryffindor).
 student(hg, 'Hermione Jean Granger', gryffindor).
 student(rw, 'Ronald Weasley', gryffindor).
@@ -15,8 +14,8 @@ student(gg, 'Gregory Goyle', slytherin).
 student(vc, 'Vincent Crabbe', slytherin).
 student(yd, 'Ying Deng', slytherin).
 
-% the teachers in Hogwarts
-% teacher(TID,TN)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% the teachers in Hogwarts %%%%%%% teacher(TID,TN)
 teacher(ad, 'Albus Percival Wulfric Brian Dumbledore').
 teacher(ff, 'Filius Flitwick').
 teacher(rh, 'Rubeus Hagrid').
@@ -35,8 +34,8 @@ teacher(sv, 'Septima Vector').
 teacher(chb, 'Charity Burbage').
 teacher(wt, 'Wilkie Twycross').
 
-% compulsory courses for the MSc in Magic
-% compCourse(SCN,CN,TID)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% compulsory courses for the MSc in Magic %%%%%%%% compCourse(SCN,CN,TID)
 compCourse(astro, 'Astronomy', as).
 compCourse(charms, 'Charms', ff).
 compCourse(defence, 'Defence against the Dark Arts', qq).
@@ -46,8 +45,8 @@ compCourse(history, 'History of Magic', cub).
 compCourse(potions, 'Potions', ss).
 compCourse(trans, 'Transfiguration', mm).
 
-% optional courses for the MSc in Magic
-% optCourse(SCN,CN,TID)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% optional courses for the MSc in Magic %%%%%%%  optCourse(SCN,CN,TID)
 optCourse(runes, 'Study of Ancient Runes', bb).
 optCourse(arith, 'Arithmancy', sv).
 optCourse(muggle, 'Muggle Studies', chb).
@@ -57,8 +56,8 @@ optCourse(app, 'Apparition', wt).
 optCourse(choir, 'Frog Choir', ff).
 optCourse(quid, 'Quidditch', mh).
 
-% already chosen courses
-% enrolled_opt(SID,SCN)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+% already chosen courses %%%%%%%  enrolled_opt(SID,SCN)
 enrolled_opt(hp,creatures).
 enrolled_opt(hp,quid).
 enrolled_opt(hp,div).
@@ -102,38 +101,105 @@ enrolled_opt(yd,runes).
 enrolled_opt(yd,arith).
 enrolled_opt(yd,choir).
 
-% enrolled(SID,SCN) -OK
-enrolled(SID,SCN):-enrolled_opt(SID,SCN).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% teaches(TN,SCN) -OK
+% 3) enrolled(SID,SCN) -OK %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+enrolled(SID,SCN):-enrolled_opt(SID,SCN).
+enrolled(SID,SCN):-compCourse(SCN,_,_),student(SID,_,_).
+
+% 4) teaches(TN,SCN) -OK %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 teaches(TN,SCN):-teacher(X,TN),compCourse(SCN,_,X).
 teaches(TN,SCN):-teacher(X,TN),optCourse(SCN,_,X).
 
-% taughtBy(SN,TN) -OK
+% 5) taughtBy(SN,TN) -OK %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 taughtBy(SN,TN):-enrolled(SID,SCN),student(SID,SN,_),teaches(TN,SCN).
 
-% takesOption(SN,CN) -OK
+% 6) takesOption(SN,CN) -OK %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 takesOption(SN,CN):-student(SID,SN,_),enrolled(SID,SCN),optCourse(SCN,CN,_).
 
-:-use_module(library(lists)). % getting the union of two lists needs this module.
+% 7) takesAllOptions(SN,OptCourses) list in alphabetical order -OK %%%%%%%%%%%%
 
-% takesAllOptions(SN,OptCourses)
-% list in alphabetical order -OK
 takesAllOptions(SN,OptCourses):-setof(CN,takesOption(SN,CN),OptCourses).
 
-% studentInHouse(House,Students)
-% list in alphabetical order of SID -OK
+% 8) studentInHouse(House,Students) list in alphabetical order of SID -OK %%%%%
+
 studentAndSID(House,SIDs):-setof(SID-SN,student(SID,SN,House),SIDs).
-studentInHouse(House,Students):-studentAndSID(House,SIDs),findall(SN,member(_-SN,SIDs),Students).
 
-% studentsOnCourse(SCN,CN,StudentsByHouse) -OK
-snSameHouseAndCourse(SCN,Housename,Students):-studentInHouse(Housename,Stu),optCourse(SCN,CN,_),setof(SN,(member(SN,Stu),takesOption(SN,CN)),Students).
-studentsOnCourse(SCN,CN,StudentsByHouse):-optCourse(SCN,CN,_),(snSameHouseAndCourse(SCN,'gryffindor',GryffindorList);\+snSameHouseAndCourse(SCN,'gryffindor',GryffindorList),GryffindorList=[]),(snSameHouseAndCourse(SCN,'hufflepuff',HufflepuffList);\+snSameHouseAndCourse(SCN,'hufflepuff',HufflepuffList),HufflepuffList=[]),(snSameHouseAndCourse(SCN,'ravenclaw',RavenclawList);\+snSameHouseAndCourse(SCN,'ravenclaw',RavenclawList),RavenclawList=[]),(snSameHouseAndCourse(SCN,'slytherin',SlytherinList);\+snSameHouseAndCourse(SCN,'slytherin',SlytherinList),SlytherinList=[]),StudentsByHouse=[gryffindor-GryffindorList,hufflepuff-HufflepuffList,ravenclaw-RavenclawList,slytherin-SlytherinList].
+studentInHouse(House,Students):-
+    studentAndSID(House,SIDs),
+    findall(SN,member(_-SN,SIDs),Students).
 
-% sharedCourse(SN1,SN2,CN) -OK
+% 9) studentsOnCourse(SCN,CN,StudentsByHouse) -OK %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+course(SCN,CN):-optCourse(SCN,CN,_);compCourse(SCN,CN,_).
+
+takesComp(SN,CN):-student(SID,SN,_),enrolled(SID,SCN),compCourse(SCN,CN,_).
+
+snSameHouseAndCourse(SCN,Housename,Students):-
+    studentInHouse(Housename,Stu),
+    compCourse(SCN,CN,_),
+    setof(SN,(member(SN,Stu),takesComp(SN,CN)),Students).
+
+snSameHouseAndCourse(SCN,Housename,Students):-
+    studentInHouse(Housename,Stu),
+    optCourse(SCN,CN,_),
+    setof(SN,(member(SN,Stu),takesOption(SN,CN)),Students).
+
+studentsG(SCN,CN,GList):-
+    course(SCN,CN),
+    (snSameHouseAndCourse(SCN,'gryffindor',GList)
+     ;
+     \+snSameHouseAndCourse(SCN,'gryffindor',GList),GList=[]
+    ).
+
+studentsH(SCN,CN,HList):-
+    course(SCN,CN),
+    (snSameHouseAndCourse(SCN,'hufflepuff',HList)
+     ;
+     \+snSameHouseAndCourse(SCN,'hufflepuff',HList),
+     HList=[]
+    ).   % I am pretty sure this looks ugly...
+
+studentsR(SCN,CN,RList):
+    -course(SCN,CN),
+    (snSameHouseAndCourse(SCN,'ravenclaw',RList)
+     ;
+     \+snSameHouseAndCourse(SCN,'ravenclaw',RList),
+     RList=[]
+    ).
+
+studentsS(SCN,CN,SList):-
+    course(SCN,CN),
+    (snSameHouseAndCourse(SCN,'slytherin',SList)
+     ;
+     \+snSameHouseAndCourse(SCN,'slytherin',SList),SList=[]
+    ).
+
+studentsOnCourse(SCN,CN,StudentsByHouse):-
+    studentsG(SCN,CN,Glist),
+    studentsH(SCN,CN,Hlist),
+    studentsR(SCN,CN,Rlist),
+    studentsS(SCN,CN,Slist),
+    StudentsByHouse=[gryffindor-Glist,hufflepuff-Hlist,ravenclaw-Rlist,slytherin-Slist].
+
+% 10> sharedCourse(SN1,SN2,CN) -OK %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 sharedCourse(SN1,SN2,CN):-takesOption(SN1,CN),takesOption(SN2,CN),SN1\=SN2.
 
-% sameOptions(SN1,SN2,Courses) -OK
+% 11> sameOptions(SN1,SN2,Courses) -OK %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 diffOptions(Courses,Six):-member(C1,Courses),\+member(C1,Six).
-sameOptions(SN1,SN2,Courses):-takesAllOptions(SN1,Courses),takesAllOptions(SN2,Courses),SN1\=SN2.
-sameOptions(HG,SN,Courses):-HG='Hermione Jean Granger',takesAllOptions(SN,Courses),SN\=HG,takesAllOptions(HG,Six),\+diffOptions(Courses,Six).
+
+sameOptions(SN1,SN2,Courses):-
+    takesAllOptions(SN1,Courses),
+    takesAllOptions(SN2,Courses),
+    SN1\=SN2.
+
+sameOptions(SN1,SN2,Courses):-
+    SN1='Hermione Jean Granger',
+    takesAllOptions(SN2,Courses),
+    SN2\=SN1,
+    takesAllOptions(SN1,Six),
+    \+diffOptions(Courses,Six).
